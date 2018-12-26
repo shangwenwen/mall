65914b81a631c06fd02ee26888f73aafbe67edf0
shangwenwen <1967553097@qq.com>
2018-12-26 19:43:43 +0800

    shang

--- src/page/common/carousel/index.js
+++ src/page/common/carousel/index.js
@@ -5,17 +5,17 @@ class Carousel {
   // 构造函数
   constructor(element, options = {}) {
     // 合并配置
-    this.config = options
-    this.options = $.extend({}, {
+    this.options = $.extend(true, {}, {
       slideType: 'default',
-      duration: 300,
       autoplay: true,
-      interval: 3000,
       dots: true,
       dotsTrigger: 'hover',
       arrowType: 'hover',
+      interval: 3000,
+      duration: 300,
       index: 0
     }, options)
+
     // 获取轮播外层
     this.element = $(element)
     // 获取轮播元素
@@ -66,22 +66,25 @@ class Carousel {
     }, options.interval)
   }
 
-  _addIndex() {
+  _addIndex(num) {
     var options = this.options
     var elementItem = this.elementItem
 
-    options.index = options.index + 1
+    var num = num || 1
+    options.index = options.index + num
+
 
     if (options.index >= elementItem.length) {
       options.index = 0
     }
   }
 
-  _subIndex() {
+  _subIndex(num) {
     var options = this.options
     var elementItem = this.elementItem
 
-    options.index = options.index - 1
+    var num = num || 1
+    options.index = options.index - num
 
     if (options.index < 0) {
       options.index = elementItem.length - 1
@@ -105,7 +108,7 @@ class Carousel {
         elementItem.eq(options.index).addClass('carousel-item-right')
       }, 50)
     } else {
-      _this._addIndex()
+      _this._addIndex(num)
       elementItem.eq(options.index).addClass('carousel-item-next')
 
       setTimeout(function() {
@@ -118,8 +121,8 @@ class Carousel {
       elementItem.removeClass('carousel-item-this carousel-item-next carousel-item-prev carousel-item-left carousel-item-right')
       elementItem.eq(options.index).addClass('carousel-item-this')
       isSliding = false
-    }, 300)
-    
+    }, 50)
+
     // 指示器焦点
     _this.elementDots.find('li').eq(options.index).addClass('active').siblings().removeClass('active')
 
@@ -167,14 +170,13 @@ class Carousel {
     var options = this.options
     var element = this.element
     var elementItem = this.elementItem
-    console.log(options.index)
+
     var dotsTpl = _this.elementDots = $([
       '<ul class="carousel-dots">',
       function() {
         var str = []
 
         $.each(elementItem, function(index, val) {
-
           str.push('<li' + (options.index === index ? ' class="active"' : '') + '></li>')
         })
 
@@ -185,12 +187,14 @@ class Carousel {
 
     element.append(dotsTpl)
 
-    dotsTpl.find('li').on('mouseover', function(){
+    dotsTpl.find('li').on('mouseover', function() {
       var index = $(this).index()
-      if(index> options.index){
-        _this.slide('add')
-      }else if(index< options.index){
-        _this.slide('sub')
+      console.log(index)
+      console.log(options.index)
+      if (index > options.index) {
+        _this.slide('add', index - options.index)
+      } else if (index < options.index) {
+        _this.slide('sub', options.index - index)
       }
     })
 
